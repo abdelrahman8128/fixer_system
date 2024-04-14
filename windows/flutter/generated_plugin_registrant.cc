9ecbd431d6b9e72b2ea692fbf212c1f05264fa0c
abdelrahman <abdelrahmank8128@gmail.com>
2024-04-14 03:51:28 +0200

    first commit

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <rive_common/rive_plugin.h>
-#include <win_toast/win_toast_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   RivePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("RivePlugin"));
-  WinToastPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WinToastPlugin"));
 }
